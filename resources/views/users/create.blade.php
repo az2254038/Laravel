f72c61d09b296f07a95258a0fbb2ed4d7944207a
vagrant <vagrant@homestead>
2019-01-17 12:32:59 +0000

    增加CSRF验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,8 @@
       </div>
       <div class="card-body">
         <form action="{{ route('users.store') }}" method="post">
+          {{ csrf_field() }}
+
           <div class="form-group">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
