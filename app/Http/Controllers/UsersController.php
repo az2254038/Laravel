a5a7a5f6efe26ac338d02b4413d2a1c7adcb8cec
vagrant <vagrant@homestead>
2019-01-21 12:14:09 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,6 +24,14 @@ class UsersController extends Controller
             'email'    => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return ;
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '欢迎加入辉辉的俱乐部~');
+        return redirect()->route('users.show', [$user]);
     }
 }
