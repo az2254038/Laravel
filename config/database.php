4755afc229b01871caf84cae91d5d3615ce9baed
vagrant <vagrant@homestead>
2019-01-22 11:14:46 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$db_config = get_db_config();
+
 return [
 
     /*
@@ -13,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+//    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
@@ -58,11 +61,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+//            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+//            'database' => env('DB_DATABASE', 'forge'),
+            'database' => $db_config['database'],
+//            'username' => env('DB_USERNAME', 'forge'),
+            'username' => $db_config['username'],
+//            'password' => env('DB_PASSWORD', ''),
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
