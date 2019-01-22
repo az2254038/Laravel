907ef22c37926553068d8f0b746e2ef6c36b8fb1
vagrant <vagrant@homestead>
2019-01-22 12:18:19 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,6 +31,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎加入辉辉的俱乐部~');
         return redirect()->route('users.show', [$user]);
     }
