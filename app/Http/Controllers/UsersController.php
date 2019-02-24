2a0111b9a3662b349a5492fedba60f46716f33d2
vagrant <vagrant@homestead>
2019-02-24 12:26:02 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,4 +36,28 @@ class UsersController extends Controller
         session()->flash('success', '欢迎加入辉辉的俱乐部~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功~');
+
+        return redirect()->route('users.show', $user->id);
+    }
 }
